13a9116bc29b397386d995e1f50d5873a01795fe
Cheesysponge <ohnoalieninvasion@gmail.com>
2024-12-07 15:42:31 -0800

    initial terrablender setup

--- src/main/java/name/crimson/Crimson.java
+++ src/main/java/name/crimson/Crimson.java
@@ -3,6 +3,7 @@ package name.crimson;
 import net.fabricmc.api.ModInitializer;
 
 public class Crimson implements ModInitializer {
+    public static final String MODID = "crimson";
     @Override
     public void onInitialize() {
 
--- src/main/java/name/crimson/terrablender/TerraMain.java
+++ src/main/java/name/crimson/terrablender/TerraMain.java
@@ -1,6 +1,13 @@
 package name.crimson.terrablender;
 
+import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class TerraMain implements TerraBlenderApi {
+
+
+    @Override
+    public void onTerraBlenderInitialized() {
+        TerraBlenderApi.super.onTerraBlenderInitialized();
+    }
 }
